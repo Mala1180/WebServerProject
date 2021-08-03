76a40dee29bf82eee14c423c9b0c359a9cda4bf5
Mattia Matteini <matteinimattia@gmail.com>
2021-08-03 16:56:21 +0200

    implemented ThreadingTCPServer

--- server.py
+++ server.py
@@ -1,60 +1,35 @@
-from socket import *
 import sys
 import signal
+import socketserver as socket
+import http.server as http
 
-port = 80
-server_socket = socket(AF_INET, SOCK_STREAM)
-server_socket.bind(("", port))
-server_socket.listen(1)
+port = 8080
+server_socket = socket.ThreadingTCPServer(("127.0.0.1", port), http.SimpleHTTPRequestHandler)
+server_socket.allow_reuse_address = True
+server_socket.daemon_threads = True
+print(server_socket.server_address)
 
-print("Web Server is listening on port %d"%(port))
+print("Web Server is listening on port %d" % port)
 
-def signal_handler(signal, frame):
-    print('You pressed Ctrl+C!')
-    server_socket.close()
-    sys.exit(0)
+def signal_handler():
+    print("Exiting server (Ctrl + C)")
+    try:
+        if server_socket:    
+            server_socket.server_close()
+    finally:
+        sys.exit(0)
+
 
 signal.signal(signal.SIGINT, signal_handler)
 
-while True:
-    print('Ready to serve...')
-    # Stabilisce la connessione, ossia sul socket si prepara ad accettare connessioni
-    # in entrata all'indirizzo e porta definiti
-    connection_socket, addr = server_socket.accept()
+try:
+    while True:
+        print('Ready to serve...')
+        server_socket.serve_forever()
 
-    try:
-        message = connection_socket.recv(1024) ## riceve il messaggio di richiesta dal client
-        # controlla che il messaggio non sia vuoto
-        if len(message.split()) > 0:
-            # stampa a video il messaggio di richiesta del client GET...
-            print(message, '::', message.split()[0], ':', message.split()[1])
-            filename = message.split()[1]
-            # poichè la seconda parte dell'intestazione HTTP include un '/',
-            # questo indica di leggere dal secondo carattere espresso attraverso '[1:]'
-            print(filename, '||', filename[1:])
-            # il server apre il file
-            f = open(filename[1:], 'r+')
-            # bufferizza l'intero contenuto del file richiesto in un buffer temporaneo
-            outputdata = f.read()
-            #print(outputdata)
-                
-            # Invia la riga di intestazione HTTP nel socket con il messaggio OK
+except KeyboardInterrupt:
+    pass
 
-            connection_socket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-            connection_socket.send(outputdata.encode())
-            connection_socket.send("\r\n".encode())
-            connection_socket.close()
 
-    except IOError:
-        # Invia messaggio di risposta per file non trovato
-        connection_socket.send(bytes("HTTP/1.1 404 Not Found\r\n\r\n","UTF-8"))
-        connection_socket.send(bytes("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n","UTF-8"))
-        connection_socket.close()        
-
-         
-server_socket.close()
-connection_socket.close()
-print("Closing Server")
-## Termina il programma dopo aver inviato i dati corrispondenti
-sys.exit()
+server_socket.server_close()
 
