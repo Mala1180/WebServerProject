50eb248c88000804e74c3ec2d4dc6bdb5dd3da23
Mattia Matteini <matteinimattia@gmail.com>
2021-08-06 12:08:37 +0200

    added balconi fioriti and san michele, modified index

--- js/article.js
+++ js/article.js
@@ -1,68 +1,72 @@
-    function goToArticle(article){
-      switch(article){
-        case 0:
-        location.href = "events/calici-di-stelle.html";
-        break;
-        case 1:
-        location.href = "events/san-martino-santarcangelo.html";
-        break;
-        case 2:
-        location.href = "events/festival-santarcangelo.html";
-        break;
+function goToArticle(article) {
+  switch (article) {
+    case 0:
+      location.href = "events/calici-di-stelle.html";
+      break;
+    case 1:
+      location.href = "events/san-martino-santarcangelo.html";
+      break;
+    case 2:
+      location.href = "events/festival-santarcangelo.html";
+      break;
+    case 3:
+      location.href = "events/balconi-fioriti.html";
+      break;
+    case 4:
+      location.href = "events/fiera-san-michele.html";
+      break;
+  }
+}
 
-      }        
-    }
 
+function downloadArticle(article) {
+  var doc = new jsPDF();
+  //doc.textSize(12);
+  doc.setFontSize(12);
+  doc.text(20, 20, pageTitle.innerHTML);
+  doc.text(20, 30, pageLittleDescription.innerHTML);
+  //doc.text(20, 45, pageDescription.innerHTML);
+  var words = pageDescription.textContent.split(" ");
+  var numberWords = words.length;
 
-    function downloadArticle(article){
-     var doc = new jsPDF();
-     //doc.textSize(12);
-     doc.setFontSize(12);
-     doc.text(20, 20, pageTitle.innerHTML);
-     doc.text(20, 30, pageLittleDescription.innerHTML);
-     //doc.text(20, 45, pageDescription.innerHTML);
-     var words = pageDescription.textContent.split(" ");
-     var numberWords = words.length;
+  var rows = [];
+  //rows[0] = "";
+  var indexRow = 0;
+  var count = 0;
+  var worldforline = 14;
 
-     var rows = [];
-     //rows[0] = "";
-     var indexRow = 0;
-     var count = 0;
-     var worldforline = 14;
-
-     while(count + (indexRow * worldforline) < numberWords) {
-      if(count < worldforline) {
-        if(count == 0){
-          rows[indexRow] = words[count + (indexRow * worldforline)] + " ";
-        }else{
-          rows[indexRow] += words[count + (indexRow * worldforline)] + " ";
-        }
-        count++;
+  while (count + (indexRow * worldforline) < numberWords) {
+    if (count < worldforline) {
+      if (count == 0) {
+        rows[indexRow] = words[count + (indexRow * worldforline)] + " ";
+      } else {
+        rows[indexRow] += words[count + (indexRow * worldforline)] + " ";
       }
-      else {
-        count = 0;
-        indexRow++;
-      }  
-     }
-
-     for(var i = 0; i < rows.length; i++){
-        doc.text(20, 40 + 10 * i, rows[i]);
-     }
-
-     console.log(rows);
-     doc.save(getTitle(article) + '.pdf');
+      count++;
+    } else {
+      count = 0;
+      indexRow++;
     }
+  }
 
-    function getTitle(article){
-        switch (article){
-            case 0:
-            return "Calici di stelle"
-            break;
-            case 1:
-            return "San Martino"
-            break;
-            case 2:
-            return "Santarcangelo Festival 2050"
-            break;
-        }
-    }
+  for (var i = 0; i < rows.length; i++) {
+    doc.text(20, 40 + 10 * i, rows[i]);
+  }
+
+  console.log(rows);
+  doc.save(getTitle(article) + '.pdf');
+}
+
+function getTitle(article) {
+  switch (article) {
+    case 0:
+      return "Calici di stelle"
+      break;
+    case 1:
+      return "San Martino"
+      break;
+    case 2:
+      return "Santarcangelo Festival 2050"
+      break;
+  }
+}
