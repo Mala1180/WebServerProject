90b849b7eb2690ffb2e1b44a5034cdfc307afd82
Alberto Paganelli <albi1600@gmail.com>
2021-08-18 18:29:06 +0200

    removed useless files and added data

--- js/article.js
+++ js/article.js
@@ -18,50 +18,61 @@ function goToArticle(article) {
   }
 }
 
+function getLabExams(username, password){
+  alert("ciao");
+}
+
+function downloadServices(){
+  var link = document.createElement('a');
+  link.href = "pdf/policlinico.pdf";
+  link.download = 'policlinico.pdf';
+  link.dispatchEvent(new MouseEvent('click'));
+}
+
 
 function downloadArticle(article) {
-  var doc = new jsPDF();
-  //doc.textSize(12);
-  doc.setFontSize(11);
-  var words = "";
-  var numberWords = 0;
-  var json = $.getJSON("articles/articles.json", function(json) {
-    var title = "", description = "", text = "";
-          title = json["Services"][article]["Title"];
-          description  = json["Services"][article]["Description"];
-          text = json["Services"][article]["Text"];
-          words = json["Services"][article]["Text"].split(" ");
-          numberWords = words.length;
-          doc.text(20, 20, title);
-          doc.text(20, 30, description);
+  // var doc = new jsPDF();
+  // //doc.textSize(12);
+  // doc.setFontSize(11);
+  // var words = "";
+  // var numberWords = 0;
+  // var json = $.getJSON("articles/articles.json", function(json) {
+  //   var title = "", description = "", text = "";
+  //         title = json["Services"][article]["Title"];
+  //         description  = json["Services"][article]["Description"];
+  //         text = json["Services"][article]["Text"];
+  //         words = json["Services"][article]["Text"].split(" ");
+  //         numberWords = words.length;
+  //         doc.text(20, 20, title);
+  //         doc.text(20, 30, description);
 
-          var rows = [];
-          var indexRow = 0;
-          var count = 0;
-          var worldforline = 15;
+  //         var rows = [];
+  //         var indexRow = 0;
+  //         var count = 0;
+  //         var worldforline = 15;
 
-          while (count + (indexRow * worldforline) < numberWords) {
-            if (count < worldforline) {
-              if (count == 0) {
-                rows[indexRow] = words[count + (indexRow * worldforline)] + " ";
-              } else {
-                rows[indexRow] += words[count + (indexRow * worldforline)] + " ";
-              }
-              count++;
-            } else {
-              count = 0;
-              indexRow++;
-            }
-          }
+  //         while (count + (indexRow * worldforline) < numberWords) {
+  //           if (count < worldforline) {
+  //             if (count == 0) {
+  //               rows[indexRow] = words[count + (indexRow * worldforline)] + " ";
+  //             } else {
+  //               rows[indexRow] += words[count + (indexRow * worldforline)] + " ";
+  //             }
+  //             count++;
+  //           } else {
+  //             count = 0;
+  //             indexRow++;
+  //           }
+  //         }
 
-          for (var i = 0; i < rows.length; i++) {
-            console.log(rows[i]);
-            doc.text(20, 40 + 6 * i, rows[i]);
-          }
+  //         for (var i = 0; i < rows.length; i++) {
+  //           console.log(rows[i]);
+  //           doc.text(20, 40 + 6 * i, rows[i]);
+  //         }
 
-          console.log(rows);
-          doc.save(getTitle(article) + '.pdf');
-  });
+  //         console.log(rows);
+  //         doc.save(getTitle(article) + '.pdf');
+  // });
 
 }
 
